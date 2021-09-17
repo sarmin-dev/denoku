86587f6138c35eaf95f501ea5b1a6c9022641b1f
sarmin-dev <89064196+sarmin-dev@users.noreply.github.com>
2021-09-17 19:04:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', event => {
   const ip = event.request.headers.get("x-forwarded-for");
-  event.respondWith(new Response({"ip":ip}, { 
+  event.respondWith(new Response(JSON.stringify({"ip":ip}), { 
     headers: [['content-type', 'text/json']],
   }));
 });
