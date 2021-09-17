e21f46e7295079023452c39141204e0bea5cd721
sarmin-dev <89064196+sarmin-dev@users.noreply.github.com>
2021-09-17 18:51:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
-    new Response("Hello world", {
+    new Response("pantek kau", {
       status: 200,
       headers: { "content-type": "text/plain" },
     }),
