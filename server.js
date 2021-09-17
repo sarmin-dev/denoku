08729dcd532de88aaff0e3b1506a234364e7d8eb
sarmin-dev <89064196+sarmin-dev@users.noreply.github.com>
2021-09-17 18:53:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response("pantek kau", {
-      status: 200,
-      headers: { "content-type": "text/plain" },
-    }),
-  );
+import 'https://deno.land/x/fetch_event_adapter/listen.ts';
+
+if (typeof FetchEvent !== 'undefined') console.log(true);
+
+self.addEventListener('fetch', event => {
+  const ip = event.request.headers.get("x-forwarded-for");
+  event.respondWith(new Response(`Hello ${ip}`, { 
+    headers: [['content-type', 'text/plain']],
+  }));
 });
