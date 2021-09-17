75487340aa92f1752cf12a205c73949a904ca918
sarmin-dev <89064196+sarmin-dev@users.noreply.github.com>
2021-09-17 18:55:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-import 'https://deno.land/x/fetch_event_adapter/listen.ts';
-
-if (typeof FetchEvent !== 'undefined') console.log(true);
-
 self.addEventListener('fetch', event => {
   const ip = event.request.headers.get("x-forwarded-for");
   event.respondWith(new Response(`Hello ${ip}`, { 
